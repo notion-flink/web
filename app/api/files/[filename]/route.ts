fb490afc9d40245b4f1eb1fc6d24d4d40562cad2
SengMitnick <sengmitnick@163.com>
2024-02-11 21:54:42 +0800

    Update route.ts

--- app/api/files/[filename]/route.ts
+++ app/api/files/[filename]/route.ts
@@ -1,26 +1,26 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import mime from 'mime';
 import { NextResponse, type NextRequest } from "next/server"
 
-export function GET(req: NextRequest,{ params }: { params: { filename: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { filename: string } }) {
   const { filename } = params;
   const filePath = path.join(
     process.cwd(),
     "public",
     filename
-  )
-  
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return NextResponse.json({ message: "File not found" }, { status: 404 })
-    }
+  );
+
+  try {
+    const data = await fs.readFile(filePath);
     const contentType = mime.getType(filePath) || 'application/octet-stream';
     return new NextResponse(data, {
       status: 200,
       headers: {
         'Content-Type': contentType
       }
-   });
-  });
+    });
+  } catch (err) {
+    return NextResponse.json({ message: "File not found" }, { status: 404 });
+  }
 }
