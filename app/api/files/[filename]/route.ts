86f872e244cb01a706b49285ecf6cb0a422d36d3
SengMitnick <sengmitnick@163.com>
2024-02-12 01:11:56 +0800

    Update route.ts

--- app/api/files/[filename]/route.ts
+++ app/api/files/[filename]/route.ts
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest, { params }: { params: { filename: st
       }
     });
   } catch (err) {
-    console.error("Error read file:", error);
+    console.error("Error read file:", err);
     return NextResponse.json({ message: "File not found" }, { status: 404 });
   }
 }
