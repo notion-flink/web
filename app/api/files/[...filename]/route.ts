618663bfe28f4770803840a36a034320060edcc9
SengMitnick <sengmitnick@163.com>
2024-02-13 00:21:15 +0800

    文件编码和类型处理

--- app/api/files/[...filename]/route.ts
+++ app/api/files/[...filename]/route.ts
@@ -1,32 +1,32 @@
-import fs from 'fs/promises';
-import path from 'path';
-import mime from 'mime';
+import fs from "fs/promises"
+import path from "path"
+import mime from "mime"
 import { NextResponse, type NextRequest } from "next/server"
 
-export async function GET(req: NextRequest, { params }: { params: { filename: string[] } }) {
-  const filename = params.filename.join('/');
-  const filePath = path.join(
-    process.cwd(),
-    "public",
-    filename
-  );
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { filename: string[] } }
+) {
+  const filename = params.filename.join("/")
+  const filePath = path.join(process.cwd(), "public", filename)
 
   try {
-    let content:  Buffer | string = await fs.readFile(filePath);
-    const contentType = mime.getType(filePath) || 'application/octet-stream';
-    
+    let content: Buffer | string = await fs.readFile(filePath)
+    let contentType = mime.getType(filePath) || "application/octet-stream"
+
     // Check if the content type is text-based, then adjust encoding
-    if (contentType.startsWith('text/') || contentType === 'application/json') {
-      content = content.toString('utf8');
+    if (contentType.startsWith("text/") || contentType === "application/json") {
+      content = content.toString("utf8")
+      contentType = `${contentType}; charset=utf-8`
     }
     return new NextResponse(content, {
       status: 200,
       headers: {
-        'Content-Type': contentType
+        "Content-Type": contentType
       }
-    });
+    })
   } catch (err) {
-    console.error("Error read file:", err);
-    return NextResponse.json({ message: "File not found" }, { status: 404 });
+    console.error("Error read file:", err)
+    return NextResponse.json({ message: "File not found" }, { status: 404 })
   }
 }
